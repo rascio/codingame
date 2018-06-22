aa92a217c5eb73b40726908a449810765036d70c
Manuel Rascioni <manuelrascioni@gmail.com>
2018-06-23 00:18:36 +0200

    base state machine

--- src/bender/episode1.clj
+++ src/bender/episode1.clj
@@ -1,6 +1,10 @@
 (ns Solution
   (:gen-class)
   (:require [clojure.string :as str]))
+(defn log [& args]
+  (binding [*out* *err*]
+    (apply println "DEBUG:" args))
+  args)
 
 (defn -parse-row [row]
     (str/split row #"|"))
@@ -10,30 +14,53 @@
     (loop [i L map []]
       (if (> i 0)
         (let [row (read-line)]
-          (binding [*out* *err*]
-              (println "DEBUG:" row))
+          (log row)
           (recur (dec i) (conj map (-parse-row row))))
         map))))
 
 (def START "@")
 (defn -find-start [map]
   (reduce
-    (fn [res line]
-      (let [{x :x y :y} res row (inc x) column (.indexOf line START)]
-        (if (= -1 y)
-          {:x row :y column :state :init}
+    (fn [{x :x y :y :as res} line]
+      (let [row (inc y) column (.indexOf line START)]
+        (if (= -1 x)
+          {:x column :y row :state :init}
           res)))
     {:x -1 :y -1}
     map))
 
-(defn -solve [{x :x y :y state :state} map])
+(defn directions [direction x y]
+  (case direction
+    :NORTH [x (dec y)]
+    :EAST  [(inc x) y]
+    :SOUTH [x (inc y)]
+    :WEST  [(dec x) y]
+    nil))
 
+(defn move [{x :x y :y :as state} dir map]
+  (let [[next-x next-y] (directions dir x y)]
+    (case (get-in map [next-x next-y])
+      " " (assoc state :dir dir :x next-x :y next-y)
+      nil)))
+
+(defn find-direction [state m & dir]
+  (->> dir
+    (map #(move state % m))
+    (map #(do (log %) %))
+    (filter some?)
+    first))
+
+(defmulti -solve (fn [state & args] (:state state)))
+(defmethod -solve :init [state map]
+  (let [next (find-direction state map :SOUTH :EAST :NORTH :WEST)]
+    (println (:dir next))))
+(defmethod -solve :default [a b]
+  (println "default" a b))
 
 (defn -main [& args]
     (let [map (-read-map)]
       (-> (-find-start map)
-        (-solve map)
-        (println "answer"))))
+        (-solve map))))
 
 (def input "10 10
 ##########
