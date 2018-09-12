013ef6c68d92830f7667f26809e7564ac4543ae1
Manuel Rascioni <manuelrascioni@gmail.com>
2018-09-12 21:58:43 +0200

    P4

--- src/l99/lists.clj
+++ src/l99/lists.clj
@@ -8,3 +8,32 @@
     (if (nil? tail)
       head
       (my-last tail)))
+
+(defn my-but-last
+  "Find the last but one box of a list.
+      Example:
+      * (my-but-last '(a b c d))
+      (C D)"
+  [[first second & tail]]
+  (if (nil? tail)
+    (seq [first second])
+    (my-but-last (cons second tail))))
+
+(defn element-at
+  "P03 (*) Find the K'th element of a list.
+        The first element in the list is number 1.
+        Example:
+        * (element-at '(a b c d e) 3)
+        C"
+  [[head & tail] index]
+  (if (= 0 index)
+    head
+    (element-at tail (dec index))))
+
+(defn length
+  "P04 (*) Find the number of elements of a list."
+  [[head & tail]]
+  (if (and (nil? tail)
+           (nil? head))
+    0
+    (inc (length tail))))
--- test/l99/lists_test.clj
+++ test/l99/lists_test.clj
@@ -2,7 +2,29 @@
   (:require [clojure.test :refer :all]
             [l99.lists :refer :all]))
 
+(defn -seq-of [& s] s)
+
 (deftest test-my-last
-  (is (= 'd (my-last '(a b c d))))
-  (is (= 'a (my-last '(a))))
-  (is (= nil (my-last nil))))
+  (is (= 4 (my-last (-seq-of 1 2 3 4))))
+  (is (= 1 (my-last (-seq-of 1))))
+  (is (= nil (my-last (-seq-of)))))
+
+(deftest test-my-but-last
+  (is (= '(3 4) (my-but-last (-seq-of 1 2 3 4))))
+  (is (= '(1 nil) (my-but-last (-seq-of 1))))
+  (is (= '(nil nil) (my-but-last '()))))
+
+(deftest test-element-at
+  (is (= 1 (element-at '(1 2 3 4) 0)))
+  (is (= 2 (element-at '(1 2 3 4) 1)))
+  (is (= 3 (element-at '(1 2 3 4) 2)))
+  (is (= 4 (element-at '(1 2 3 4) 3)))
+  (is (= nil (element-at '() 0)))
+  (is (= nil (element-at '(1) 1))))
+
+(deftest test-length
+  (is (= 0 (length (-seq-of))))
+  (is (= 1 (length (-seq-of 1))))
+  (is (= 2 (length (-seq-of 1 2))))
+  (is (= 3 (length (-seq-of 1 2 3))))
+  (is (= 4 (length (-seq-of 1 2 3 4)))))
